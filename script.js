7054d0c759d2b2d94f1d7ea8c2fe97d6f7c27a8b
Azael <azael.hrdz.101@gmail.com>
2025-02-13 20:12:38 -0600

    Agregar inf a imag

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 alert("¡Bienvenido a mi página web!");
+
+// ocultar info de imagen
+document.getElementById('myImage').addEventListener('click', function() {
+    var info = document.getElementById('info');
+    if (info.classList.contains('hidden')) {
+        info.classList.remove('hidden');
+    } else {
+        info.classList.add('hidden');
+    }
+});
