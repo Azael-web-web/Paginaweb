22beb5702938394e82dd90e46cc74c5ad47a2182
Azael <azael.hrdz.101@gmail.com>
2025-02-14 16:28:44 -0600

    modificacion

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ document.getElementById('myImage').addEventListener('click', function() {
         info.classList.add('hidden');
     }
 });
+
+document.getElementById('esfera').addEventListener('click', function() {
+    document.getElementById('info').innerText = 'Has seleccionado la Esfera.';
+    document.getElementById('audio').style.display = 'block';
+    document.getElementById('audio').play();
+});
+
+document.getElementById('foco').addEventListener('click', function() {
+    document.getElementById('info').innerText = 'Has seleccionado el Foco.';
+    document.getElementById('audio').style.display = 'block';
+    document.getElementById('audio').play();
+});
